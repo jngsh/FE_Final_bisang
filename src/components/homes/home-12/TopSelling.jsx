5c24f3203fba506ebe32b13942374ed5494fc00f
jngsh <tse8ce17@gmail.com>
2024-07-31 09:28:45 +0900

    feat: Add select top selling products

--- src/components/homes/home-12/TopSelling.jsx
+++ src/components/homes/home-12/TopSelling.jsx
@@ -1,6 +1,187 @@
+// import Star from "@/components/common/Star";
+// import { useContextElement } from "@/context/Context";
+// import { products26 } from "@/data/products/grocery";
+// import { Link } from "react-router-dom";
+// import { Autoplay, Pagination } from "swiper/modules";
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// export default function TopSelling() {
+//   const { toggleWishlist, isAddedtoWishlist } = useContextElement();
+//   const { setQuickViewItem } = useContextElement();
+//   const { addProductToCart, isAddedToCartProducts } = useContextElement();
+//   const swiperOptions = {
+//     autoplay: {
+//       delay: 5000,
+//     },
+//     modules: [Autoplay, Pagination],
+//     slidesPerView: 8,
+//     slidesPerGroup: 1,
+//     effect: "none",
+//     loop: false,
+//     pagination: {
+//       el: "#top_selling_carousel .slideshow-pagination",
+//       type: "bullets",
+//       clickable: true,
+//     },
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 2,
+//         slidesPerGroup: 2,
+//         spaceBetween: 16,
+//       },
+//       768: {
+//         slidesPerView: 3,
+//         slidesPerGroup: 1,
+//         spaceBetween: 22,
+//       },
+//       992: {
+//         slidesPerView: 4,
+//         slidesPerGroup: 1,
+//         spaceBetween: 28,
+//       },
+//       1200: {
+//         slidesPerView: 5,
+//         slidesPerGroup: 1,
+//         spaceBetween: 34,
+//       },
+//     },
+//   };
+//   return (
+//     <section className="product-carousel container">
+//       <div className="d-flex align-items-center justify-content-center justify-content-md-between flex-wrap mb-3 pb-xl-2 mb-xl-4 gap-4">
+//         <h2 className="section-title fw-normal">Top Selling Products</h2>
+//         <Link
+//           className="btn-link btn-link_md default-underline text-uppercase fw-medium"
+//           to="/shop-1"
+//         >
+//           See All Products
+//         </Link>
+//       </div>
+
+//       <div id="top_selling_carousel" className="position-relative">
+//         <Swiper
+//           {...swiperOptions}
+//           className="swiper-container js-swiper-slider"
+//         >
+//           {products26.map((elm, i) => (
+//             <SwiperSlide
+//               key={i}
+//               className="swiper-slide product-card product-card_style9 border rounded-3"
+//             >
+//               <div className="position-relative pb-3">
+//                 <div className="pc__img-wrapper pc__img-wrapper_wide3">
+//                   <Link to={`/product1_simple/${elm.id}`}>
+//                     <img
+//                       loading="lazy"
+//                       src={elm.imgSrc}
+//                       width="253"
+//                       height="198"
+//                       alt="Cropped Faux leather Jacket"
+//                       className="pc__img"
+//                     />
+//                   </Link>
+//                 </div>
+//                 <div className="anim_appear-bottom position-absolute w-100 text-center">
+//                   <button
+//                     className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-add-cart js-open-aside"
+//                     onClick={() => addProductToCart(elm.id)}
+//                     title={
+//                       isAddedToCartProducts(elm.id)
+//                         ? "Already Added"
+//                         : "Add to Cart"
+//                     }
+//                   >
+//                     <svg
+//                       className="d-inline-block"
+//                       width="14"
+//                       height="14"
+//                       viewBox="0 0 20 20"
+//                       fill="none"
+//                       xmlns="http://www.w3.org/2000/svg"
+//                     >
+//                       <use
+//                         href={`${
+//                           isAddedToCartProducts(elm.id)
+//                             ? "#icon_cart_added"
+//                             : "#icon_cart"
+//                         }`}
+//                       />
+//                     </svg>
+//                   </button>
+//                   <button
+//                     className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-quick-view"
+//                     data-bs-toggle="modal"
+//                     data-bs-target="#quickView"
+//                     onClick={() => setQuickViewItem(elm)}
+//                     title="Quick view"
+//                   >
+//                     <svg
+//                       className="d-inline-block"
+//                       width="18"
+//                       height="18"
+//                       viewBox="0 0 18 18"
+//                       xmlns="http://www.w3.org/2000/svg"
+//                     >
+//                       <use href="#icon_view" />
+//                     </svg>
+//                   </button>
+//                   <button
+//                     className={`btn btn-round btn-hover-red border-0 text-uppercase js-add-wishlist ${
+//                       isAddedtoWishlist(elm.id) ? "active" : ""
+//                     }`}
+//                     onClick={() => toggleWishlist(elm.id)}
+//                     title="Add To Wishlist"
+//                   >
+//                     <svg
+//                       width="14"
+//                       height="14"
+//                       viewBox="0 0 20 20"
+//                       fill="none"
+//                       xmlns="http://www.w3.org/2000/svg"
+//                     >
+//                       <use href="#icon_heart" />
+//                     </svg>
+//                   </button>
+//                 </div>
+//               </div>
+
+//               <div className="pc__info position-relative">
+//                 <p className="pc__category">{elm.category}</p>
+//                 <h6 className="pc__title">
+//                   <Link to={`/product1_simple/${elm.id}`}>{elm.title}</Link>
+//                 </h6>
+//                 <div className="product-card__review d-sm-flex align-items-center">
+//                   <div className="reviews-group d-flex">
+//                     <Star stars={elm.stars} />
+//                   </div>
+//                   <span className="reviews-note text-lowercase text-secondary ms-sm-1">
+//                     {elm.reviews}
+//                   </span>
+//                 </div>
+//                 <div className="product-card__price d-flex">
+//                   <span className="money price fs-5">${elm.price}</span>
+//                 </div>
+//               </div>
+//             </SwiperSlide>
+//           ))}
+
+//           {/* <!-- /.swiper-wrapper --> */}
+//         </Swiper>
+//         {/* <!-- /.swiper-container js-swiper-slider --> */}
+
+//         <div className="slideshow-pagination mt-4 d-flex align-items-center justify-content-center"></div>
+//         {/* <!-- /.products-pagination --> */}
+//       </div>
+//       {/* <!-- /.position-relative --> */}
+//     </section>
+//   );
+// }
+
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Star from "@/components/common/Star";
 import { useContextElement } from "@/context/Context";
-import { products26 } from "@/data/products/grocery";
 import { Link } from "react-router-dom";
 import { Autoplay, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -9,6 +190,9 @@ export default function TopSelling() {
   const { toggleWishlist, isAddedtoWishlist } = useContextElement();
   const { setQuickViewItem } = useContextElement();
   const { addProductToCart, isAddedToCartProducts } = useContextElement();
+
+  const [products, setProducts] = useState([]);
+
   const swiperOptions = {
     autoplay: {
       delay: 5000,
@@ -46,6 +230,20 @@ export default function TopSelling() {
       },
     },
   };
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:8090/bisang/home/top-selling-products");
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <section className="product-carousel container">
       <div className="d-flex align-items-center justify-content-center justify-content-md-between flex-wrap mb-3 pb-xl-2 mb-xl-4 gap-4">
@@ -63,20 +261,20 @@ export default function TopSelling() {
           {...swiperOptions}
           className="swiper-container js-swiper-slider"
         >
-          {products26.map((elm, i) => (
+          {products.map((elm, i) => (
             <SwiperSlide
               key={i}
               className="swiper-slide product-card product-card_style9 border rounded-3"
             >
               <div className="position-relative pb-3">
                 <div className="pc__img-wrapper pc__img-wrapper_wide3">
-                  <Link to={`/product1_simple/${elm.id}`}>
+                  <Link to={`/product1_simple/${elm.productId}`}>
                     <img
                       loading="lazy"
-                      src={elm.imgSrc}
+                      src={elm.productImage || "No Image"} // 기본 이미지
                       width="253"
                       height="198"
-                      alt="Cropped Faux leather Jacket"
+                      alt={elm.productName || "No Name"}
                       className="pc__img"
                     />
                   </Link>
@@ -84,9 +282,9 @@ export default function TopSelling() {
                 <div className="anim_appear-bottom position-absolute w-100 text-center">
                   <button
                     className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-add-cart js-open-aside"
-                    onClick={() => addProductToCart(elm.id)}
+                    onClick={() => addProductToCart(elm.productId)}
                     title={
-                      isAddedToCartProducts(elm.id)
+                      isAddedToCartProducts(elm.productId)
                         ? "Already Added"
                         : "Add to Cart"
                     }
@@ -101,7 +299,7 @@ export default function TopSelling() {
                     >
                       <use
                         href={`${
-                          isAddedToCartProducts(elm.id)
+                          isAddedToCartProducts(elm.productId)
                             ? "#icon_cart_added"
                             : "#icon_cart"
                         }`}
@@ -127,9 +325,9 @@ export default function TopSelling() {
                   </button>
                   <button
                     className={`btn btn-round btn-hover-red border-0 text-uppercase js-add-wishlist ${
-                      isAddedtoWishlist(elm.id) ? "active" : ""
+                      isAddedtoWishlist(elm.productId) ? "active" : ""
                     }`}
-                    onClick={() => toggleWishlist(elm.id)}
+                    onClick={() => toggleWishlist(elm.productId)}
                     title="Add To Wishlist"
                   >
                     <svg
@@ -146,20 +344,20 @@ export default function TopSelling() {
               </div>
 
               <div className="pc__info position-relative">
-                <p className="pc__category">{elm.category}</p>
+                <p className="pc__category">{elm.category || ""}</p>
                 <h6 className="pc__title">
-                  <Link to={`/product1_simple/${elm.id}`}>{elm.title}</Link>
+                  <Link to={`/product1_simple/${elm.productId}`}>{elm.productName || "No Name"}</Link>
                 </h6>
                 <div className="product-card__review d-sm-flex align-items-center">
                   <div className="reviews-group d-flex">
                     <Star stars={elm.stars} />
                   </div>
                   <span className="reviews-note text-lowercase text-secondary ms-sm-1">
-                    {elm.reviews}
+                    {elm.reviews || "No Reviews"}
                   </span>
                 </div>
                 <div className="product-card__price d-flex">
-                  <span className="money price fs-5">${elm.price}</span>
+                  <span className="money price fs-5">{elm.productPrice || "0"}원</span>
                 </div>
               </div>
             </SwiperSlide>
