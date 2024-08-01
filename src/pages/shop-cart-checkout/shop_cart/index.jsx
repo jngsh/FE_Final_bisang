259cccdb3854178d1ca517bf3c15fd2b3d21a653
Parkminji <parkminji5787@gmail.com>
2024-08-01 15:39:43 +0900

    feat: Add cart information

--- src/pages/shop-cart-checkout/shop_cart/index.jsx
+++ src/pages/shop-cart-checkout/shop_cart/index.jsx
@@ -15,7 +15,7 @@ export default function ShopCartPage() {
       <main className="page-wrapper">
         <div className="mb-4 pb-4"></div>
         <section className="shop-checkout container">
-          <h2 className="page-title">Cart</h2>
+          <h2 className="page-title">장바구니</h2>
           <ChectoutSteps />
           <Cart />
         </section>
