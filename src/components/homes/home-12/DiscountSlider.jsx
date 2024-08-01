5777c6b749d737aedbe33349ad5c75f117c1e016
jngsh <tse8ce17@gmail.com>
2024-08-01 09:28:52 +0900

    feat: Add DiscountEvent

--- src/components/homes/home-12/DiscountSlider.jsx
+++ src/components/homes/home-12/DiscountSlider.jsx
@@ -1,14 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Star from "@/components/common/Star";
 import { useContextElement } from "@/context/Context";
-import { products27 } from "@/data/products/grocery";
-import { Link } from "react-router-dom";
-import { Autoplay } from "swiper/modules";
+import openEvent from "./images/openEvent.png";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
 export default function DiscountSlider() {
+  const [discounts, setDiscounts] = useState([]);
   const { toggleWishlist, isAddedtoWishlist } = useContextElement();
   const { setQuickViewItem } = useContextElement();
   const { addProductToCart, isAddedToCartProducts } = useContextElement();
+
+  useEffect(() => {
+    axios.get(`http://localhost:8090/bisang/home/discounts/2`)
+      .then(response => {
+        setDiscounts(response.data);
+      })
+      .catch(error => {
+        console.error("데이터를 가져오는 중 오류 발생:", error);
+      });
+  }, []);
+
+  const formatPrice = (price) => {
+    return new Intl.NumberFormat().format(price);
+  };
+
   const swiperOptions = {
     autoplay: {
       delay: 5000,
@@ -41,6 +58,7 @@ export default function DiscountSlider() {
       },
     },
   };
+
   return (
     <section className="discount-carousel container">
       <div className="d-flex align-items-center justify-content-center justify-content-md-between flex-wrap mb-3 pb-xl-2 mb-xl-4 gap-4">
@@ -53,14 +71,16 @@ export default function DiscountSlider() {
             <div
               className="background-img"
               style={{
-                backgroundImage: "url(/assets/images/home/demo12/deal-bg.jpg)",
+                backgroundImage: `url(${openEvent})`,
               }}
             ></div>
-            <div className="position-absolute position-center text-white text-center w-100">
-              <h2 className="section-title fw-bold text-white">$20</h2>
-              <h3 className="text-white fw-normal">Under Products</h3>
-              <p>Limited Time Only</p>
-            </div>
+            {discounts.length > 0 && (
+              <div className="position-absolute position-center text-white text-center w-100">
+                <h2 className="section-title fw-bold text-white">{(discounts[0].discountRate * 100).toFixed(0)}%</h2>
+                <h3 className="text-white fw-normal">{discounts[0].discountType}</h3>
+                <p>{discounts[0].startDate} - {discounts[0].endDate}</p>
+              </div>
+            )}
           </div>
         </div>
         <div className="col-sm-6 col-md-8 col-lg-9 col-xl-80per">
@@ -69,113 +89,122 @@ export default function DiscountSlider() {
               {...swiperOptions}
               className="swiper-container js-swiper-slider"
             >
-              {products27.map((elm, i) => (
-                <SwiperSlide
-                  key={i}
-                  className="swiper-slide product-card product-card_style9 border rounded-3"
-                >
-                  <div className="position-relative pb-3">
-                    <div className="pc__img-wrapper pc__img-wrapper_wide3">
-                      <Link to={`/product1_simple/${elm.id}`}>
-                        <img
-                          loading="lazy"
-                          src={elm.imgSrc}
-                          width="253"
-                          height="198"
-                          alt="Cropped Faux leather Jacket"
-                          className="pc__img"
-                        />
-                      </Link>
-                    </div>
-                    <div className="anim_appear-bottom position-absolute w-100 text-center">
-                      <button
-                        className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-add-cart js-open-aside"
-                        onClick={() => addProductToCart(elm.id)}
-                        title={
-                          isAddedToCartProducts(elm.id)
-                            ? "Already Added"
-                            : "Add to Cart"
-                        }
-                      >
-                        <svg
-                          className="d-inline-block"
-                          width="14"
-                          height="14"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <use
-                            href={`${
-                              isAddedToCartProducts(elm.id)
-                                ? "#icon_cart_added"
-                                : "#icon_cart"
+              {discounts.length > 0 ? (
+                discounts.map((product, i) => {
+                  const discountedPrice = (product.productPrice * (1 - product.discountRate)).toFixed(0);
+                  
+                  return (
+                    <SwiperSlide
+                      key={product.productId}
+                      className="swiper-slide product-card product-card_style9 border rounded-3"
+                    >
+                      <div className="position-relative pb-3">
+                        <div className="pc__img-wrapper pc__img-wrapper_wide3">
+                          <a href={`/product1_simple/${product.productId}`}>
+                            <img
+                              loading="lazy"
+                              src={product.productImage}
+                              width="253"
+                              height="198"
+                              alt={product.productName}
+                              className="pc__img"
+                            />
+                          </a>
+                        </div>
+                        <div className="anim_appear-bottom position-absolute w-100 text-center">
+                          <button
+                            className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-add-cart js-open-aside"
+                            onClick={() => addProductToCart(product.productId)}
+                            title={
+                              isAddedToCartProducts(product.productId)
+                                ? "Already Added"
+                                : "Add to Cart"
+                            }
+                          >
+                            <svg
+                              className="d-inline-block"
+                              width="14"
+                              height="14"
+                              viewBox="0 0 20 20"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <use
+                                href={`${
+                                  isAddedToCartProducts(product.productId)
+                                    ? "#icon_cart_added"
+                                    : "#icon_cart"
+                                }`}
+                              />
+                            </svg>
+                          </button>
+                          <button
+                            className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-quick-view"
+                            data-bs-toggle="modal"
+                            data-bs-target="#quickView"
+                            onClick={() => setQuickViewItem(product)}
+                            title="Quick view"
+                          >
+                            <svg
+                              className="d-inline-block"
+                              width="18"
+                              height="18"
+                              viewBox="0 0 18 18"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <use href="#icon_view" />
+                            </svg>
+                          </button>
+                          <button
+                            className={`btn btn-round btn-hover-red border-0 text-uppercase js-add-wishlist ${
+                              isAddedtoWishlist(product.productId) ? "active" : ""
                             }`}
-                          />
-                        </svg>
-                      </button>
-                      <button
-                        className="btn btn-round btn-hover-red border-0 text-uppercase me-2 js-quick-view"
-                        data-bs-toggle="modal"
-                        data-bs-target="#quickView"
-                        onClick={() => setQuickViewItem(elm)}
-                        title="Quick view"
-                      >
-                        <svg
-                          className="d-inline-block"
-                          width="18"
-                          height="18"
-                          viewBox="0 0 18 18"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <use href="#icon_view" />
-                        </svg>
-                      </button>
-                      <button
-                        className={`btn btn-round btn-hover-red border-0 text-uppercase js-add-wishlist ${
-                          isAddedtoWishlist(elm.id) ? "active" : ""
-                        }`}
-                        onClick={() => toggleWishlist(elm.id)}
-                        title="Add To Wishlist"
-                      >
-                        <svg
-                          width="14"
-                          height="14"
-                          viewBox="0 0 20 20"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <use href="#icon_heart" />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-
-                  <div className="pc__info position-relative">
-                    <p className="pc__category">{elm.category}</p>
-                    <h6 className="pc__title">
-                      <Link to={`/product1_simple/${elm.id}`}>{elm.title}</Link>
-                    </h6>
-                    <div className="product-card__review d-sm-flex align-items-center">
-                      <div className="reviews-group d-flex">
-                        <Star stars={elm.rating} />
+                            onClick={() => toggleWishlist(product.productId)}
+                            title="Add To Wishlist"
+                          >
+                            <svg
+                              width="14"
+                              height="14"
+                              viewBox="0 0 20 20"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <use href="#icon_heart" />
+                            </svg>
+                          </button>
+                        </div>
                       </div>
-                      <span className="reviews-note text-lowercase text-secondary ms-sm-1">
-                        {elm.reviews}
-                      </span>
-                    </div>
-                    <div className="product-card__price d-flex">
-                      <span className="money price fs-5">${elm.price}</span>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              ))}
 
-              {/* <!-- /.swiper-wrapper --> */}
+                      <div className="pc__info position-relative">
+                        <p className="pc__category">{product.categoryId}</p>
+                        <h6 className="pc__title">
+                          <a href={`/product1_simple/${product.productId}`}>{product.productName}</a>
+                        </h6>
+                        <div className="product-card__review d-sm-flex align-items-center">
+                          <div className="reviews-group d-flex">
+                            <Star stars={product.stars} />
+                          </div>
+                          <span className="reviews-note text-lowercase text-secondary ms-sm-1">
+                            {product.reviews || "No Reviews"}
+                          </span>
+                        </div>
+                        <div className="product-card__price d-flex">
+                          <span className="old-price me-2 text-decoration-line-through">
+                            {formatPrice(product.productPrice)}원
+                          </span>
+                          <span className="new-price fs-5">
+                            {formatPrice(discountedPrice)}원
+                          </span>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  );
+                })
+              ) : (
+                <p>No discounts available.</p>
+              )}
             </Swiper>
-            {/* <!-- /.swiper-container js-swiper-slider --> */}
           </div>
-          {/* <!-- /.position-relative --> */}
         </div>
       </div>
     </section>
