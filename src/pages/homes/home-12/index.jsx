36bab10676df7d377fd4f7d4e1b184e9ba0a2f7c
rosa.soulee <162273058+Soul-LEE@users.noreply.github.com>
2024-08-02 10:05:35 +0900

    fix: Change page title

--- src/pages/homes/home-12/index.jsx
+++ src/pages/homes/home-12/index.jsx
@@ -13,8 +13,8 @@ import TopSelling from "@/components/homes/home-12/TopSelling";
 import MetaComponent from "@/components/common/MetaComponent";
 import Header1 from "@/components/headers/Header1";
 const metadata = {
-  title: "Home 12 || Uomo eCommerce Reactjs Template",
-  description: "Uomo eCommerce Reactjs Template",
+  title: "피터펫 :: PETER PET",
+  // description: "Uomo eCommerce Reactjs Template",
 };
 export default function HomePage12() {
   return (
