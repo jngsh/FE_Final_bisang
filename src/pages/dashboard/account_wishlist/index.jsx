4bc1626fb69c6a0553e67f797cdbfc5dac05c4ba
rosa.soulee <162273058+Soul-LEE@users.noreply.github.com>
2024-08-02 09:53:07 +0900

    fix: Update index.jsx

--- src/pages/dashboard/account_wishlist/index.jsx
+++ src/pages/dashboard/account_wishlist/index.jsx
@@ -1,4 +1,4 @@
-import Footer1 from "@/components/footers/Footer1.jsx" ;omponents/footers/Footer1.jsx" ;
+import Footer1 from "@/components/footers/Footer1.jsx";
 
 import Header1 from "@/components/headers/Header1";
 import AccountWishlist from "@/components/otherPages/AccountWishlist";
