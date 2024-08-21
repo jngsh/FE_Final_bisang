f2c9ae9280863fe8b0d3d09083e26d2c00822348
rosa.soulee <162273058+Soul-LEE@users.noreply.github.com>
2024-08-21 17:16:02 +0900

    fix: Change mobileFooter1's icons

--- src/components/footers/MyPageBtn.jsx
+++ src/components/footers/MyPageBtn.jsx
@@ -7,16 +7,14 @@ export const MyPageBtn = () =>{
             to="/account_dashboard"
             className="footer-mobile__link d-flex flex-column align-items-center"
           >
-            <svg
-              className="d-block"
+            <img
+              src="/assets/images/mobilefooter1/mypage.png"
               width="18"
               height="18"
               viewBox="0 0 18 18"
               fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            />
               <use href="#icon_hanger" />
-            </svg>
             <span>MyPage</span>
           </Link>
         </div>
