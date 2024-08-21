760d5026f93d37c865c98338b1d2157ea33fca68
rosa.soulee <162273058+Soul-LEE@users.noreply.github.com>
2024-08-21 14:18:11 +0900

    fix: Modify order details table

--- src/components/otherPages/OrderDetails.jsx
+++ src/components/otherPages/OrderDetails.jsx
@@ -42,7 +42,7 @@ const OrderDetails = () => {
     return (
         <div className="col-lg-9">
             <div className="page-content my-account__orders-list">
-                
+                <div className="table-wrapper">
                     <table className="aboveTable" border={1}>
                         <tbody>
                             <tr><td style={{ textAlign: "center", fontWeight:"bold" }}>✅ 주문 번호</td><td style={{ textAlign: "center", fontWeight:"bold" }}>✅ 주문 일자</td><td style={{ textAlign: "center", fontWeight:"bold" }}>&nbsp;✅ 총 결제금액</td></tr>
@@ -51,16 +51,15 @@ const OrderDetails = () => {
                             <td style={{ textAlign: "center" }}> {formatCurrency(totalAmount)} 원</td></tr>
                         </tbody>
                     </table>
-                
+                    </div>
                 <div className="page-content my-account__orders-list">
                     <table className="orders-table">
-                        <thead className="orders-header">
+                        <thead>
                             <tr>
                                 <th>no.</th>
                                 <th colSpan={2}>상품명</th>
-                                <th>단가(원)</th>
-                                <th>수량</th>
-                                <th>금액(원)</th>
+                                <th>단가<br/>수량</th>
+                                <th>금액</th>
                                 <th>리뷰</th>
                             </tr>
                         </thead>
@@ -68,11 +67,15 @@ const OrderDetails = () => {
                             {orderDetails.map((items, i) => (
                                 <tr key={i}>
                                     <td>{i + 1}</td>
-                                    <td><img className="productImage" src={items.productImage} alt="Product" /></td>
-                                    <td>{items.productName}</td>
-                                    <td>{formatCurrency(items.productPrice)}</td>
-                                    <td>{items.amount}</td>
-                                    <td>{formatCurrency(items.totalPrice)}</td>
+                                    <td><img src={items.productImage} alt="Product" /></td>
+                                    <td >{items.productName}</td>
+                                    <td>
+                                        <div>{formatCurrency(items.productPrice)}원</div> 
+                                    <div>           {items.amount}개</div>
+
+                                    </td>
+                                   
+                                    <td>{formatCurrency(items.totalPrice)}원</td>
                                     <td><button className="button" onClick={goToReview}>리뷰 작성</button></td>
                                 </tr>
                             ))}
