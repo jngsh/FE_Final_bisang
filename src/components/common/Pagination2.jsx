cef96f503e415d6649525bdc16dc81b79fdc41bc
jngsh <tse8ce17@gmail.com>
2024-08-23 13:52:55 +0900

    fix: Hide prev and next buttons when pagination has few items

--- src/components/common/Pagination2.jsx
+++ src/components/common/Pagination2.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Pagination2({ totalPages = 4, currentPage, onPageChange }) {
+export default function Pagination2({ totalPages, currentPage, onPageChange }) {
   const [pageOffset, setPageOffset] = useState(0);
 
   const handlePageClick = (pageNumber) => {
@@ -25,20 +25,22 @@ export default function Pagination2({ totalPages = 4, currentPage, onPageChange 
   }
 
   return (
-    <nav className="shop-pages d-flex justify-content-between mt-3" aria-label="Page navigation">
-      <a
-        href="#"
-        className={`btn-link d-inline-flex align-items-center ${currentPage === 1 ? 'disabled' : ''}`}
-        onClick={(e) => {
-          e.preventDefault();
-          handlePrevClick();
-        }}
-      >
-        <svg className="me-1" width="7" height="11" viewBox="0 0 7 11" xmlns="http://www.w3.org/2000/svg">
-          <use href="#icon_prev_sm" />
-        </svg>
-        <span className="fw-medium">PREV</span>
-      </a>
+    <nav className="shop-pages d-flex justify-content-center mt-3" aria-label="Page navigation">
+      {totalPages > 4 &&
+        <a
+          href="#"
+          className={`btn-link d-inline-flex align-items-center ${currentPage === 1 ? 'disabled' : ''}`}
+          onClick={(e) => {
+            e.preventDefault();
+            handlePrevClick();
+          }}
+        >
+          <svg className="me-1" width="7" height="11" viewBox="0 0 7 11" xmlns="http://www.w3.org/2000/svg">
+            <use href="#icon_prev_sm" />
+          </svg>
+          <span className="fw-medium">이전</span>
+        </a>
+      }
       <ul className="pagination mb-0">
         {pageNumbers.map((pageNumber) => (
           <li key={pageNumber} className="page-item">
@@ -55,19 +57,21 @@ export default function Pagination2({ totalPages = 4, currentPage, onPageChange 
           </li>
         ))}
       </ul>
-      <a
-        href="#"
-        className={`btn-link d-inline-flex align-items-center ${currentPage === totalPages ? 'disabled' : ''}`}
-        onClick={(e) => {
-          e.preventDefault();
-          handleNextClick();
-        }}
-      >
-        <span className="fw-medium me-1">NEXT</span>
-        <svg width="7" height="11" viewBox="0 0 7 11" xmlns="http://www.w3.org/2000/svg">
-          <use href="#icon_next_sm" />
-        </svg>
-      </a>
+      {totalPages > 4 &&
+        <a
+          href="#"
+          className={`btn-link d-inline-flex align-items-center ${currentPage === totalPages ? 'disabled' : ''}`}
+          onClick={(e) => {
+            e.preventDefault();
+            handleNextClick();
+          }}
+        >
+          <span className="fw-medium me-1">다음</span>
+          <svg width="7" height="11" viewBox="0 0 7 11" xmlns="http://www.w3.org/2000/svg">
+            <use href="#icon_next_sm" />
+          </svg>
+        </a>
+      }
     </nav>
   );
 }
