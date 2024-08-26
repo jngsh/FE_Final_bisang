2ac10d0b30b52a3b0e3ba009f5ac9b91f36acbd1
jngsh <tse8ce17@gmail.com>
2024-08-27 00:00:26 +0900

    design: Improved hero images on the home page

--- src/components/homes/home-12/data/heroData.js
+++ src/components/homes/home-12/data/heroData.js
@@ -1,39 +1,37 @@
 import { useTranslation } from 'react-i18next';
-import dogAndCat1 from '../images/dogandcat1.jpeg';
-import dogAndCat2 from '../images/dogandcat2.png';
-import dogAndCat3 from '../images/dogandcat3.png';
 
 export function HeroData() {
   const { t } = useTranslation();
+  const pattern = "/assets/images/homeimages/hero/pattern.png";
 
   return [
     {
       id: 1,
-      backgroundColor: "#f5e6e0",
-      imageUrl: dogAndCat1,
-      // imgUrl2: dogAndCat1,
+      backgroundColor: "#b0d49b",
+      imageUrl: pattern,
+      imgUrl2: "/assets/images/homeimages/hero/dogandcat1.png",
       alt: "Pattern",
-      title: "",
+      title: "PETER PET",
       subtitle1: t('subtitle1'),
       subtitle2: t('everyday'),
     },
     {
       id: 2,
-      backgroundColor: "#f5e6e0",
-      imageUrl: dogAndCat2,
-      // imgUrl2: dogAndCat2,
+      backgroundColor: "#ffc0cb",
+      imageUrl: pattern,
+      imgUrl2: "/assets/images/homeimages/hero/dogandcat2.png",
       alt: "Pattern",
-      title: "",
+      title: "PETER PET",
       subtitle1: t('subtitle2'),
       subtitle2: t('everyday'),
     },
     {
       id: 3,
-      backgroundColor: "#f5e6e0",
-      imageUrl: dogAndCat3,
-      // imgUrl2: dogAndCat3,
+      backgroundColor: "#add8e6",
+      imageUrl: pattern,
+      imgUrl2: "/assets/images/homeimages/hero/dogandcat3.png",
       alt: "Pattern",
-      title: "",
+      title: "PETER PET",
       subtitle1: t('subtitle3'),
       subtitle2: t('everyday'),
     }
